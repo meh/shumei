8768763f257a04c71af7552527b121ca65326382
meh <meh@schizofreni.co>
2020-10-27 22:04:23 +0100

    docs: add documentation for protocol

--- src/protocol.ts
+++ src/protocol.ts
@@ -1,4 +1,7 @@
 export namespace Wire {
+  /**
+   * Types that can be serialized through a `postMessage`.
+   */
   export type Serializable =
     | bigint
     | boolean
@@ -24,16 +27,25 @@ export namespace Wire {
     | Plain
     | Encoded;
 
+  /**
+   * The type of packet.
+   */
   export const enum Type {
     PLAIN,
     ENCODED,
   }
 
+  /**
+   * Values that do not need any encoding.
+   */
   export interface Plain {
     type: Type.PLAIN;
     value: Serializable;
   }
 
+  /**
+   * Values that have been encoded with special handling.
+   */
   export interface Encoded {
     type: Type.ENCODED;
     codec: string;
@@ -43,17 +55,27 @@ export namespace Wire {
   export type Value = Plain | Encoded;
 }
 
+/**
+ * This interface is used to support custom encoding and decoding of non-POJO values
+ * (like, functions, channels, etc.).
+ */
 export interface Codec<E, S extends Wire.Serializable, D> {
   canHandle(value: unknown): value is E;
   encode(value: E): [S, Transferable[]];
   decode(value: S): D;
 }
 
+/**
+ * Map of registered codecs.
+ */
 export const codecs = new Map<
   string,
   Codec<unknown, Wire.Serializable, unknown>
 >();
 
+/**
+ * Register a new codec, the name is used as discriminant.
+ */
 export function codec<E, S extends Wire.Serializable, D>(
   name: string,
   codec: Codec<E, S, D>
@@ -62,12 +84,22 @@ export function codec<E, S extends Wire.Serializable, D>(
   return codec;
 }
 
+/**
+ * Map of registered transferables.
+ */
 const transfers = new WeakMap<any, Transferable[]>();
+
+/**
+ * Register a set of transferables for a specific value.
+ */
 export function transfer(value: any, transfer: Transferable[]) {
   transfers.set(value, transfer);
   return value;
 }
 
+/**
+ * Encode a value.
+ */
 export function encode(value: any): [Wire.Value, Transferable[]] {
   for (const [name, codec] of codecs) {
     if (codec.canHandle(value)) {
@@ -82,6 +114,9 @@ export function encode(value: any): [Wire.Value, Transferable[]] {
   return [{ type: Wire.Type.PLAIN, value: value }, transfers.get(value) || []];
 }
 
+/**
+ * Decode a value.
+ */
 export function decode(wire: Wire.Value): any {
   switch (wire.type) {
     case Wire.Type.ENCODED:
